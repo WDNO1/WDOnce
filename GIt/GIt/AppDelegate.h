1a01c64db199bcb1d8d0d7830f6bfe0730a1813a
王东 <WD@wangdongdemacbook-air.local>
2016-05-29 16:06:38 +0800

    这是第一次上传的代码

---
+++ GIt/GIt/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GIt
+//
+//  Created by 王东 on 16/5/29.
+//  Copyright © 2016年 王东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GIt/GIt/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GIt
+//
+//  Created by 王东 on 16/5/29.
+//  Copyright © 2016年 王东. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
